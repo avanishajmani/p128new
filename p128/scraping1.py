fb8aee5803a6c5c286de9492eba82da90c7e8291
avanishajmani <77194020+avanishajmani@users.noreply.github.com>
2022-08-29 21:10:37 +0100

    Add files via upload

---
+++ p128/scraping1.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import time
+import csv
+
+START_URL = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+
+wiki = requests.get(START_URL)
+print(wiki)
+
+soup = BeautifulSoup(wiki.text, "html.parser")
+
+star_table = soup.find_all('table')
+print(len(star_table))
+
+temp_list= []
+table_rows = star_table[4].find_all('tr')
+for tr in table_rows:
+    td = tr.find_all("td")
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+print(temp_list)
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+
+for i in range(1,len(temp_list)):
+    
+    Star_names.append(temp_list[i][0])
+    Distance.append(temp_list[i][5])
+    Mass.append(temp_list[i][7])
+    Radius.append(temp_list[i][8])
+
+df = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
+print(df)
+
+df.to_csv('dwarfs.csv')
